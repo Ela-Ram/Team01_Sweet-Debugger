6d8b3ce9d8742f30805b921d45db14dc54fc87f2
Christy <j.christypriya@gmail.com>
2025-07-04 01:58:03 -0500

    code cleanup

--- src/test/java/pageObject/PremiumUserExercise_Page.java
+++ src/test/java/pageObject/PremiumUserExercise_Page.java
@@ -1,14 +1,11 @@
 package pageObject;
 
 
-import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import common.Helper;
 import common.TestContext;
